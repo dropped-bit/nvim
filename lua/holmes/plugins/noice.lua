85d5f10e68fe4b00fe0ddf3dbda3cfebb81d82ba
Martin Holmes <m.holmes.de@gmail.com>
2024-11-09 11:57:41 +0100

    nice change to noice setup. back in action :)

--- lua/holmes/plugins/noice.lua
+++ lua/holmes/plugins/noice.lua
@@ -11,10 +11,13 @@ return {
 		-- OPTIONAL:
 		--   `nvim-notify` is only needed, if you want to use the notification view.
 		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
+		-- "rcarriga/nvim-notify",
 	},
 	config = function()
 		require("noice").setup({
+			cmdline = {
+				view = "cmdline",
+			},
 			lsp = {
 				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 				override = {
@@ -26,15 +29,15 @@ return {
 			-- you can enable a preset for easier configuration
 			presets = {
 				bottom_search = true, -- use a classic bottom cmdline for search
-				command_palette = true, -- position the cmdline and popupmenu together
+				command_palette = false, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = false, -- add a border to hover docs and signature help
 			},
 		})
-		require("notify").setup({
-			background_colour = "#000",
-			render = "compact",
-		})
+		-- require("notify").setup({
+		-- 	background_colour = "#000",
+		-- 	render = "compact",
+		-- })
 	end,
 }
